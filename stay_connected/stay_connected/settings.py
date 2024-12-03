2172475797d5e41526b4c8d22fcb139121809094
root <root@app01.prod.stayconnected.lol>
2024-12-03 18:46:30 +0000

    Change container names from dev to prod

--- stay_connected/stay_connected/settings.py
+++ stay_connected/stay_connected/settings.py
@@ -171,7 +171,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'https://cdn.stayconnected.lol/commonstatic/'
 
 STATIC_ROOT = BASE_DIR / 'commonstatic/'
 
