6e2fdbc532f8ea04cfc61e2050621e542970f9cc
peikra <96653696+peikra@users.noreply.github.com>
2024-12-03 10:53:02 +0400

    Update settings.py

--- stay_connected/stay_connected/settings.py
+++ stay_connected/stay_connected/settings.py
@@ -61,7 +61,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.contrib.auth.middleware.AuthenticationMiddleware',
